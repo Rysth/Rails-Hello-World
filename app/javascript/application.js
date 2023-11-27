2c5d1be952579dd18f0285f64fe58027c803de31
John Palacios <johnpalacios.t@gmail.com>
2023-11-27 11:30:41 -0500

    Install and configurate react-router-dom

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Greeting from './components/Greeting/Greeting';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Greeting />,
+  },
+]);
 
 function App() {
   return (
     <>
       <h1>Hello World</h1>
-      <Greeting />
+      <RouterProvider router={router} />
     </>
   );
 }
